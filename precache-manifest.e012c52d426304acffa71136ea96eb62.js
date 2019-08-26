d03b9e280b198024194a83f737f83a00443d38de
feroz101 <ferozmujawar101@gmail.com>
2019-08-26 19:43:07 +0530

    Updates

---
+++ precache-manifest.e012c52d426304acffa71136ea96eb62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9dad1dd763c2a871272ca575abbf79",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "6ca5b85bbc58f68d411e",
+    "url": "/profile/static/css/main.3b4b4aab.chunk.css"
+  },
+  {
+    "revision": "f39c7bfaf4ca1ce21428",
+    "url": "/profile/static/js/2.6124002c.chunk.js"
+  },
+  {
+    "revision": "6ca5b85bbc58f68d411e",
+    "url": "/profile/static/js/main.7d550f65.chunk.js"
+  },
+  {
+    "revision": "ee2e41f31e84105b2fd9",
+    "url": "/profile/static/js/runtime~main.e2a809a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
