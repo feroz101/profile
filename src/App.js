9f220759f300241335fc6f420761115dcb9c58b3
feroz101 <ferozmujawar101@gmail.com>
2019-08-27 08:56:11 +0530

    fix vulnerability

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
         Thanks for Coming!
         <p> Profile Will Display Soon...</p>
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
